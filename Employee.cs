04e87503836d60093148e5bc10922c722ded8c7a
kennylee38 <kennylee38@gmail.com>
2023-08-12 14:32:33 -0700

    OperatorsAssignment

---
+++ Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+// Define the Employee class with properties Id, FirstName, and LastName.
+public class Employee
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    // Overload the "==" operator to compare Employee objects by their Id property.
+    public static bool operator ==(Employee emp1, Employee emp2)
+    {
+        if (ReferenceEquals(emp1, emp2))
+            return true;
+
+        if (emp1 is null || emp2 is null)
+            return false;
+
+        return emp1.Id == emp2.Id;
+    }
+
+    // Overload the "!=" operator by using the opposite of the "==" operator.
+    public static bool operator !=(Employee emp1, Employee emp2)
+    {
+        return !(emp1 == emp2);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Instantiate two Employee objects and assign values to their properties.
+        Employee emp1 = new Employee
+        {
+            Id = 1,
+            FirstName = "John",
+            LastName = "Doe"
+        };
+
+        Employee emp2 = new Employee
+        {
+            Id = 2,
+            FirstName = "Jane",
+            LastName = "Smith"
+        };
+
+        // Compare the Employee objects using the overloaded operators.
+        if (emp1 == emp2)
+        {
+            Console.WriteLine("Employee objects are equal.");
+        }
+        else
+        {
+            Console.WriteLine("Employee objects are not equal.");
+        }
+
+        // Compare the Employee objects using the overloaded operators.
+        if (emp1 != emp2)
+        {
+            Console.WriteLine("Employee objects are not equal.");
+        }
+        else
+        {
+            Console.WriteLine("Employee objects are equal.");
+        }
+    }
+}
+
+
