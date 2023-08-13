6a71a2d48a720922a8b4db3245f49926ab4956ca
kennylee38 <kennylee38@gmail.com>
2023-08-13 12:04:04 -0700

    UploadStructAssignment

--- Program.cs
+++ Program.cs
@@ -1,44 +1,21 @@
 ﻿using System;
 
+struct Number
+{
+    public decimal Amount { get; set; }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        // Instantiate two Employee objects and assign values to their properties.
-        Employee emp1 = new Employee
-        {
-            Id = 1,
-            FirstName = "John",
-            LastName = "Doe"
-        };
+        // Create an object of data type Number
+        Number numberObject = new Number();
 
-        Employee emp2 = new Employee
-        {
-            Id = 2,
-            FirstName = "Jane",
-            LastName = "Smith"
-        };
+        // Assign an amount to the Number object's Amount property
+        numberObject.Amount = 123.45m;
 
-        // Compare the Employee objects using the overloaded operators.
-        if (emp1 == emp2)
-        {
-            Console.WriteLine("Employee objects are equal.");
-        }
-        else
-        {
-            Console.WriteLine("Employee objects are not equal.");
-        }
-
-        // Compare the Employee objects using the overloaded operators.
-        if (emp1 != emp2)
-        {
-            Console.WriteLine("Employee objects are not equal.");
-        }
-        else
-        {
-            Console.WriteLine("Employee objects are equal.");
-        }
+        // Print the amount to the console
+        Console.WriteLine($"The amount is: {numberObject.Amount}");
     }
 }
-
-
