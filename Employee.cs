273468c5156ac7ae8969e68736a10035166af692
kennylee38 <kennylee38@gmail.com>
2023-08-13 13:46:23 -0700

    LamdbaExpressionAssignment

--- Employee.cs
+++ Employee.cs
@@ -1,27 +1,13 @@
 ﻿using System;
 
-// Define the Employee class with properties Id, FirstName, and LastName.
-public class Employee
+namespace LambdaExpressionAssignment
 {
-    public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-
-    // Overload the "==" operator to compare Employee objects by their Id property.
-    public static bool operator ==(Employee emp1, Employee emp2)
+    //Create an Employee class with id, firstname and lastname properties.
+    public class Employee
     {
-        if (ReferenceEquals(emp1, emp2))
-            return true;
-
-        if (emp1 is null || emp2 is null)
-            return false;
-
-        return emp1.Id == emp2.Id;
-    }
-
-    // Overload the "!=" operator by using the opposite of the "==" operator.
-    public static bool operator !=(Employee emp1, Employee emp2)
-    {
-        return !(emp1 == emp2);
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
+
--- Program.cs
+++ Program.cs
@@ -1,21 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq; // Import the namespace for LINQ
 
-struct Number
-{
-    public decimal Amount { get; set; }
-}
+namespace LamdaExpressionAssigment
 
 class Program
 {
     static void Main(string[] args)
     {
-        // Create an object of data type Number
-        Number numberObject = new Number();
+        //In the Main() method, create a list of at least 10 employees.
+        //At least two employees should have the first name "Joe".
+        List<Employee> campus = new List<Employee>()
+        {
+            new Employee { Id = 1, FirstName = "Joe", LastName = "Schmo" },
+            new Employee { Id = 2, FirstName = "Joe", LastName = "Thomas" },
+            new Employee { Id = 3, FirstName = "JimBob", LastName = "Coot" },
+            new Employee { Id = 4, FirstName = "Danny", LastName = "Dimes" },
+            new Employee { Id = 5, FirstName = "SaQuads", LastName = "Barkley" },
+            new Employee { Id = 6, FirstName = "George", LastName = "Title" },
+            new Employee { Id = 7, FirstName = "Joe", LastName = "Joesephson" },
+            new Employee { Id = 8, FirstName = "Maria", LastName = "Schmo" },
+            new Employee { Id = 9, FirstName = "Maria", LastName = "Rodriguez" },
+            new Employee { Id = 10, FirstName = "Stephanie", LastName = "Stevens" }
+        };
 
-        // Assign an amount to the Number object's Amount property
-        numberObject.Amount = 123.45m;
+        // Using foreach loop, create a new list of all employees with the first name "Joe".
+        List<Employee> joes = new List<Employee>();
+        foreach (Employee employee in campus)
+        {
+            if (employee.firstName == "Joe")
+            {
+                joes.Add(employee);
+            }
+        }
 
-        // Print the amount to the console
-        Console.WriteLine($"The amount is: {numberObject.Amount}");
+        //Do the same thing again, but this time with a lambda expression.
+        List<Employee> joes2 = campus.Where(x => x.FirstName == "Joe").ToList();
+        //Using a lambda expression, make a list of all employees with an Id number greater than 5
+        List<Employee> bigId = campus.Where(x => x.Id > 5).ToList();
+        Console.ReadLine();
+       
+        }
     }
 }
