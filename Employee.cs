6a3a862ebc879b29b69fddf171f47c0737536119
kennylee38 <kennylee38@gmail.com>
2023-08-17 15:53:06 -0700

    UploadOperator2Assignment

--- Employee.cs
+++ Employee.cs
@@ -1,13 +1,34 @@
-﻿using System;
+﻿sing System;
 
-namespace LambdaExpressionAssignment
+public class Employee
 {
-    //Create an Employee class with id, firstname and lastname properties.
-    public class Employee
+    // Properties
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    // Overload the == operator to compare Employee objects by their Id property
+    public static bool operator ==(Employee employee1, Employee employee2)
     {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        if (ReferenceEquals(employee1, null) && ReferenceEquals(employee2, null))
+            return true;
+
+        if (ReferenceEquals(employee1, null) || ReferenceEquals(employee2, null))
+            return false;
+
+        return employee1.Id == employee2.Id;
+    }
+
+    // Overload the != operator based on the == operator
+    public static bool operator !=(Employee employee1, Employee employee2)
+    {
+        return !(employee1 == employee2);
     }
 }
+
+
+
+
+
+
 
--- Program.cs
+++ Program.cs
@@ -1,22 +1,21 @@
 ﻿using System;
 
-namespace MethodClassAssignment
+class Program
 {
-     class Program
-    {
     static void Main(string[] args)
+    {
+        // Instantiate two Employee objects
+        Employee employee1 = new Employee { Id = 1, FirstName = "John", LastName = "Doe" };
+        Employee employee2 = new Employee { Id = 2, FirstName = "Jane", LastName = "Smith" };
 
-        {
-        // Instantiate the MathOperation class
-        MathOperation mathOperation = new MathOperation();
+        // Compare the Employee objects using the overloaded == operator
+        bool areEqual = employee1 == employee2;
+        bool areNotEqual = employee1 != employee2;
 
-        // Call the method in the class, passing in two numbers
-        mathOperation.PerformMathOperation(10, 20);
-
-        // Call the method in the class, specifying the parameters by name
-        mathOperation.PerformMathOperation(firstNumber: 15, secondNumber: 25);
-        }
+        // Display the comparison results
+        Console.WriteLine($"Are employee1 and employee2 equal? {areEqual}");
+        Console.WriteLine($"Are employee1 and employee2 not equal? {areNotEqual}");
     }
-
 }
+
 
