6c3dbed4b87aaa6f0690d116b763f0cd3aacbac4
kennylee38 <kennylee38@gmail.com>
2023-08-23 10:09:21 -0700

    uploadNewsletterAppMVC

---
+++ NewsletterAppMVC/ViewModel/SignupVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewsletterAppMVC.ViewModel
+{
+    public class SignupVM
+    {
+        pubic string FirstName { get; set; }
+        pubic string LastName { get; set; }
+        pubic string EmailAddress { get; set; }
+    }
+}
